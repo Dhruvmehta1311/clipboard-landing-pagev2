ed35d6e09d6d9dae88369ef709571093bae60536
Dhruv Mehta <flashxx001@gmail.com>
2024-08-07 11:53:56 +0530

    Minor Improvements.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -30,15 +30,17 @@ const Content = () => {
           iOS apps will help you organize everything.
         </p>
       </section>
-      <section className="pt-10 flex flex-col">
+      <section className="pt-10 flex flex-col lg:flex-row">
         <img src={imageComputer} alt="" />
         <div className="max-w-[400px] pt-20 w-[90%] mx-auto  flex  flex-col gap-16">
           {features.map((feature) => (
             <div className="flex flex-col gap-4" key={feature.title}>
-              <h3 className="text-xl font-semibold text-gray-700 text-center">
+              <h3 className="text-xl font-semibold text-gray-700 text-center sm:text-left">
                 {feature.title}
               </h3>
-              <p className="text-gray-400 text-center">{feature.description}</p>
+              <p className="text-gray-400 text-center sm:text-left">
+                {feature.description}
+              </p>
             </div>
           ))}
         </div>
