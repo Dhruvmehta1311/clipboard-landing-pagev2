5be67dffa05a8b251cacb15d49fcb3f534a5ce66
Dhruv Mehta <flashxx001@gmail.com>
2024-08-08 04:54:21 +0530

    Changed Shadow for Blue Button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,7 +4,7 @@ const Buttons = () => {
       <button className="text-white bg-strongCyan px-4 py-3 rounded-full drop-shadow-cyan-md-hsl w-[90%] sm:w-fit font-semibold">
         Download for iOS
       </button>
-      <button className="text-white bg-lightBlue px-4 py-3 rounded-full drop-shadow-cyan-md-hsl w-[90%] sm:w-fit font-semibold">
+      <button className="text-white bg-lightBlue px-4 py-3 rounded-full drop-shadow-blue-md-hsl w-[90%] sm:w-fit font-semibold">
         Download for MAC
       </button>
     </div>
