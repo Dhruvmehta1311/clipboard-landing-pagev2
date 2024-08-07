f3bd9879e77324a54ee418598c1ce9708a88efa1
Dhruv Mehta <flashxx001@gmail.com>
2024-08-08 04:57:09 +0530

    Changed width for Bottom Component.

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -2,7 +2,7 @@ import Buttons from "./Buttons";
 
 const Bottom = () => {
   return (
-    <section className="flex flex-col gap-8">
+    <section className="flex flex-col gap-8 max-w-[630px] w-[90%] mx-auto">
       <div className="flex flex-col gap-8">
         <p className="font-baiJamjuree text-2xl font-semibold text-gray-700 text-center">
           Clipboard for iOS and Mac OS
